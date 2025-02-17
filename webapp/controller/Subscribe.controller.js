65ed1f4d8cc8a6514aee8f84c8a451e4928f479f
Legati Matteo <58668065+legatimatteo@users.noreply.github.com>
2025-02-17 23:43:10 +0100

    Login e Subscribe pages

---
+++ webapp/controller/Subscribe.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define(
+	["./BaseController", "sap/ui/dom/includeStylesheet"],
+	function (BaseController, includeStyleSheet) {
+		"use strict";
+
+		return BaseController.extend("chartwallet.controller.Subscribe", {
+			onInit: function () {
+				// apply content density mode to root view
+				this.getView().addStyleClass(
+					this.getOwnerComponent().getContentDensityClass(),
+				);
+
+				// apply css
+				includeStyleSheet("../style/login.css");
+			},
+		});
+	},
+);
